2fefd5aab881d95a573ffcce95bc7c5fa33d84be
Mumo Nzinga <mnzinga888@gmail.com>
2019-04-23 11:43:12 +0300

    final app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuoteDisplayComponent } from './quote-display/quote-display.component';
+import { VotingComponent } from './voting/voting.component';
+import { HighlightDirective } from './highlight.directive';
+import { DatePipe } from './date.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    QuoteDisplayComponent,
+    VotingComponent,
+    HighlightDirective,
+    DatePipe
   ],
   imports: [
     BrowserModule,
